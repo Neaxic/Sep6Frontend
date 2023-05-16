5fce2aa3cd93795b32bf32bd128dc2d5bef4645f
Andre <andreas79001@gmail.com>
2023-05-16 14:38:33 +0200

    linking header + megamenu work

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -14,9 +14,21 @@ import {
   ScrollArea,
   rem,
   Text,
+  ThemeIcon,
+  SimpleGrid,
+  Anchor,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconChevronDown } from "@tabler/icons-react";
+import {
+  IconBook,
+  IconChartPie3,
+  IconChevronDown,
+  IconCode,
+  IconCoin,
+  IconFingerprint,
+  IconNotification,
+} from "@tabler/icons-react";
+import { Link } from "react-router-dom";
 
 const useStyles = createStyles((theme) => ({
   link: {
@@ -87,10 +99,63 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+const mockdata = [
+  {
+    icon: IconCode,
+    title: "Open source",
+    description: "This Pokémon’s cry is very loud and distracting",
+  },
+  {
+    icon: IconCoin,
+    title: "Free for everyone",
+    description: "The fluid of Smeargle’s tail secretions changes",
+  },
+  {
+    icon: IconBook,
+    title: "Documentation",
+    description: "Yanma is capable of seeing 360 degrees without",
+  },
+  {
+    icon: IconFingerprint,
+    title: "Security",
+    description: "The shell’s rounded shape and the grooves on its.",
+  },
+  {
+    icon: IconChartPie3,
+    title: "Analytics",
+    description: "This Pokémon uses its flying ability to quickly chase",
+  },
+  {
+    icon: IconNotification,
+    title: "Notifications",
+    description: "Combusken battles with the intensely hot flames it spews",
+  },
+];
+
 export function HeaderMenu() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
     useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+
   const { classes, theme } = useStyles();
+
+  const links = mockdata.map((item) => (
+    <UnstyledButton className={classes.subLink} key={item.title}>
+      <Group noWrap align="flex-start">
+        <ThemeIcon size={34} variant="default" radius="md">
+          <item.icon size={rem(22)} color={theme.fn.primaryColor()} />
+        </ThemeIcon>
+        <div>
+          <Text size="sm" fw={500}>
+            {item.title}
+          </Text>
+          <Text size="xs" color="dimmed">
+            {item.description}
+          </Text>
+        </div>
+      </Group>
+    </UnstyledButton>
+  ));
 
   return (
     <Box>
@@ -112,9 +177,9 @@ export function HeaderMenu() {
             spacing={0}
             className={classes.hiddenMobile}
           >
-            <a href="#" className={classes.link}>
+            <UnstyledButton className={classes.link} component={Link} to="/">
               Home
-            </a>
+            </UnstyledButton>
             <HoverCard
               width={600}
               position="bottom"
@@ -126,24 +191,58 @@ export function HeaderMenu() {
                 <a href="#" className={classes.link}>
                   <Center inline>
                     <Box component="span" mr={5}>
-                      Features
+                      Movies
                     </Box>
-                    <IconChevronDown />
+                    <IconChevronDown
+                      size={16}
+                      color={theme.fn.primaryColor()}
+                    />
                   </Center>
                 </a>
               </HoverCard.Target>
+
+              <HoverCard.Dropdown sx={{ overflow: "hidden" }}>
+                <Group position="apart" px="md">
+                  <Text fw={500}>Features</Text>
+                  <Anchor href="#" fz="xs">
+                    View all
+                  </Anchor>
+                </Group>
+
+                <Divider
+                  my="sm"
+                  mx="-md"
+                  color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+                />
+
+                <SimpleGrid cols={2} spacing={0}>
+                  {links}
+                </SimpleGrid>
+
+                <div className={classes.dropdownFooter}>
+                  <Group position="apart">
+                    <div>
+                      <Text fw={500} fz="sm">
+                        Get started
+                      </Text>
+                      <Text size="xs" color="dimmed">
+                        Their food sources have decreased, and their numbers
+                      </Text>
+                    </div>
+                    <Button variant="default">Get started</Button>
+                  </Group>
+                </div>
+              </HoverCard.Dropdown>
             </HoverCard>
-            <a href="#" className={classes.link}>
-              Learn
-            </a>
-            <a href="#" className={classes.link}>
-              Academy
-            </a>
           </Group>
 
           <Group className={classes.hiddenMobile}>
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+            <Button variant="default" component={Link} to="login">
+              Log in
+            </Button>
+            <Button component={Link} to="signup">
+              Sign up
+            </Button>
           </Group>
 
           <Burger
@@ -169,9 +268,9 @@ export function HeaderMenu() {
             color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
           />
 
-          <a href="#" className={classes.link}>
+          <UnstyledButton className={classes.link} component={Link} to="/">
             Home
-          </a>
+          </UnstyledButton>
           <UnstyledButton className={classes.link}>
             <Center inline>
               <Box component="span" mr={5}>
