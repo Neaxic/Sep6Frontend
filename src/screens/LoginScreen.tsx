78994e3556ad3671f4675fc989b71b28afd03c87
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-05-16 16:30:12 +0200

    Login og sårn noget
    Jaja det godt du

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -12,12 +12,28 @@ import {
 } from "@mantine/core";
 import * as React from "react";
 import { Link } from "react-router-dom";
-
+import { useForm } from "@mantine/form";
 export interface LoginScreenProps {
   //Props goes here
 }
 
 export const LoginScreen = ({ ...props }: LoginScreenProps) => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      password: (val) =>
+        val.length <= 6
+          ? "Password should include at least 6 characters"
+          : null,
+    },
+  });
+  const handleFormSubmit = () => {
+    console.log(form.values); // Log the form object
+  };
   return (
     <Container size={420} my={40}>
       <Title
@@ -32,25 +48,50 @@ export const LoginScreen = ({ ...props }: LoginScreenProps) => {
       <Text color="dimmed" size="sm" align="center" mt={5}>
         Do not have an account yet? <a href="/signup"> Sign Up here</a>
       </Text>
-
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Group position="apart" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl" component={Link} to="/">
-          Sign in
-        </Button>
-      </Paper>
+      <form onSubmit={form.onSubmit(() => {})}>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput
+            label="Email"
+            placeholder="you@mantine.dev"
+            value={form.values.email}
+            onChange={(event) =>
+              form.setFieldValue("email", event.currentTarget.value)
+            }
+            error={form.errors.email && "Invalid email"}
+            radius="md"
+            required
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+            value={form.values.password}
+            onChange={(event) =>
+              form.setFieldValue("password", event.currentTarget.value)
+            }
+            error={
+              form.errors.password &&
+              "Password should include at least 6 characters"
+            }
+          />
+          <Group position="apart" mt="lg">
+            <Checkbox label="Remember me" />
+            <Anchor component="button" size="sm">
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button
+            onClick={handleFormSubmit}
+            fullWidth
+            mt="xl"
+            component={Link}
+            to="/"
+          >
+            Sign in
+          </Button>
+        </Paper>
+      </form>
     </Container>
   );
 };
